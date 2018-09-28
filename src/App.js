a8002d5d06be4a02b53f57e3de07cf0bcb74d996
Andrew Park <apark11278@gmail.com>
2018-09-27 23:13:16 -0400

    Updated component renders to one line using map. Adjusted background image

--- src/App.js
+++ src/App.js
@@ -65,21 +65,9 @@ class App extends Component {
         </div>
         <div className="container">
           <div className="form-row">
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[0]].id} alt={Fighters[this.state.image[0]].name} image={Fighters[this.state.image[0]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[1]].id} alt={Fighters[this.state.image[1]].name} image={Fighters[this.state.image[1]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[2]].id} alt={Fighters[this.state.image[2]].name} image={Fighters[this.state.image[2]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[3]].id} alt={Fighters[this.state.image[3]].name} image={Fighters[this.state.image[3]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[4]].id} alt={Fighters[this.state.image[4]].name} image={Fighters[this.state.image[4]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[5]].id} alt={Fighters[this.state.image[5]].name} image={Fighters[this.state.image[5]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[6]].id} alt={Fighters[this.state.image[6]].name} image={Fighters[this.state.image[6]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[7]].id} alt={Fighters[this.state.image[7]].name} image={Fighters[this.state.image[7]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[8]].id} alt={Fighters[this.state.image[8]].name} image={Fighters[this.state.image[8]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[9]].id} alt={Fighters[this.state.image[9]].name} image={Fighters[this.state.image[9]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[10]].id} alt={Fighters[this.state.image[10]].name} image={Fighters[this.state.image[10]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[11]].id} alt={Fighters[this.state.image[11]].name} image={Fighters[this.state.image[11]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[12]].id} alt={Fighters[this.state.image[12]].name} image={Fighters[this.state.image[12]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[13]].id} alt={Fighters[this.state.image[13]].name} image={Fighters[this.state.image[13]].image} />
-            <Streetfighter onClick={this.handleClickChange} id={Fighters[this.state.image[14]].id} alt={Fighters[this.state.image[14]].name} image={Fighters[this.state.image[14]].image} />
+            {this.state.image.map(image => {
+              return(<Streetfighter onClick={this.handleClickChange} id={Fighters[image].id} alt={Fighters[image].name} image={Fighters[image].image} />)
+            })}
           </div>
         </div>
 
